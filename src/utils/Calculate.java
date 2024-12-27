5351a7ae540e0ac8e4640539d81fe414109c4bdf
Justine Agcanas <agcanasjustine1@gmail.com>
2024-12-27 20:52:44 +0800

    fix: typo

--- src/utils/Calculate.java
+++ src/utils/Calculate.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class Calculate {
-	public int overDueDate(LocalDate dateBorrowed, LocalDate currentDate) {
+	public int overDueDays(LocalDate dateBorrowed, LocalDate currentDate) {
 		Period period = Period.between(dateBorrowed, currentDate);
 		return period.getDays();
 	}
